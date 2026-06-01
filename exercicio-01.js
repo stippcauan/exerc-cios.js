95684b8a38b6e173c6fc30783438114caf31ee63
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2026-06-01 17:30:32 +0000

    exercicios prontos.

---
+++ exercicio-01.js
@@ -0,0 +1,17 @@
+let nota1 = 36;
+let nota2 = 80;
+let nota3 = 78;
+
+let media = (nota1 + nota2 + nota3) / 3
+
+if (media >7){
+    console.log("O aluno está aprovado")
+}
+if(media >= 5 && media < 7){
+    console.log("O aluno está no se liga")    
+}
+if(media <= 5){
+     console.log("O aluno está reprovado")
+}
+
+console.log("Tem a média: " + media);
---
+++ exercicio-02.js
@@ -0,0 +1,10 @@
+let velocidade = 88;
+
+if (velocidade >80){
+    console.log("Você foi multado por excesso de velocidade!")
+}
+if(velocidade <80){
+    console.log("Velocidade permitida. Boa viagem!")
+}
+
+console.log("Tem a velocidade: " + velocidade);
